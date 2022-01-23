6fb05a5957b81fa70e821daf913a87fd77b240db
isoboye24 <isobarguy4real@gmail.com>
2022-01-23 14:54:53 +0200

    I setup the structure for the app

---
+++ src/component/All-product.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AllProducts = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-1-of-4">I am a col 1</div>
+        <div className="col-1-of-4">I am a col 2</div>
+        <div className="col-1-of-4">I am a col 3</div>
+        <div className="col-1-of-4">I am a col 4</div>
+      </div>
+    </>
+  );
+};
+
+export default AllProducts;
---
+++ src/data.js
@@ -0,0 +1,286 @@
+export const product = [
+  {
+    id: 1,
+    image: "images/product.png",
+    stockStatus: "in stock",
+    price: "301.88",
+    clothName: "Soho Coat",
+    buttons: [
+      {
+        id: 1,
+        buttonColor: "#FFFFFF",
+      },
+      {
+        id: 2,
+        buttonColor: "#DD7878",
+      },
+      {
+        id: 3,
+        buttonColor: "#C41212",
+      },
+      {
+        id: 4,
+        buttonColor: "#3D8CAE",
+      },
+      {
+        id: 5,
+        buttonColor: "#63913F",
+      },
+      {
+        id: 6,
+        buttonColor: "#FBAC5A",
+      },
+      {
+        id: 7,
+        buttonColor: "#cc33ff",
+      },
+      {
+        id: 8,
+        buttonColor: "#00ff00",
+      },
+    ],
+    sizes: [
+      {
+        id: 1,
+        size: "xxs",
+      },
+      {
+        id: 2,
+        size: "xs",
+      },
+      {
+        id: 3,
+        size: "s",
+      },
+      {
+        id: 4,
+        size: "m",
+      },
+      {
+        id: 5,
+        size: "l",
+      },
+      {
+        id: 6,
+        size: "xl",
+      },
+      {
+        id: 7,
+        size: "xxl",
+      },
+    ],
+  },
+  {
+    id: 2,
+    image: "images/dealdoodle.jpg",
+    stockStatus: "in stock",
+    price: "301.88",
+    clothName: "Dealdoodle Dress",
+    buttons: [
+      {
+        id: 1,
+        buttonColor: "#FFFFFF",
+      },
+      {
+        id: 2,
+        buttonColor: "#DD7878",
+      },
+      {
+        id: 3,
+        buttonColor: "#C41212",
+      },
+      {
+        id: 4,
+        buttonColor: "#3D8CAE",
+      },
+      {
+        id: 5,
+        buttonColor: "#63913F",
+      },
+      {
+        id: 6,
+        buttonColor: "#FBAC5A",
+      },
+      {
+        id: 7,
+        buttonColor: "#cc33ff",
+      },
+      {
+        id: 8,
+        buttonColor: "#00ff00",
+      },
+    ],
+    sizes: [
+      {
+        id: 1,
+        size: "xxs",
+      },
+      {
+        id: 2,
+        size: "xs",
+      },
+      {
+        id: 3,
+        size: "s",
+      },
+      {
+        id: 4,
+        size: "m",
+      },
+      {
+        id: 5,
+        size: "l",
+      },
+      {
+        id: 6,
+        size: "xl",
+      },
+      {
+        id: 7,
+        size: "xxl",
+      },
+    ],
+  },
+  {
+    id: 3,
+    image: "images/curtis.jpg",
+    stockStatus: "in stock",
+    price: "301.88",
+    clothName: "Curtis Dress",
+    buttons: [
+      {
+        id: 1,
+        buttonColor: "#FFFFFF",
+      },
+      {
+        id: 2,
+        buttonColor: "#DD7878",
+      },
+      {
+        id: 3,
+        buttonColor: "#C41212",
+      },
+      {
+        id: 4,
+        buttonColor: "#3D8CAE",
+      },
+      {
+        id: 5,
+        buttonColor: "#63913F",
+      },
+      {
+        id: 6,
+        buttonColor: "#FBAC5A",
+      },
+      {
+        id: 7,
+        buttonColor: "#cc33ff",
+      },
+      {
+        id: 8,
+        buttonColor: "#00ff00",
+      },
+    ],
+    sizes: [
+      {
+        id: 1,
+        size: "xxs",
+      },
+      {
+        id: 2,
+        size: "xs",
+      },
+      {
+        id: 3,
+        size: "s",
+      },
+      {
+        id: 4,
+        size: "m",
+      },
+      {
+        id: 5,
+        size: "l",
+      },
+      {
+        id: 6,
+        size: "xl",
+      },
+      {
+        id: 7,
+        size: "xxl",
+      },
+    ],
+  },
+  {
+    id: 4,
+    image: "images/cowgirl.jpg",
+    stockStatus: "in stock",
+    price: "301.88",
+    clothName: "cowgirl Dress",
+    buttons: [
+      {
+        id: 1,
+        buttonColor: "#FFFFFF",
+      },
+      {
+        id: 2,
+        buttonColor: "#DD7878",
+      },
+      {
+        id: 3,
+        buttonColor: "#C41212",
+      },
+      {
+        id: 4,
+        buttonColor: "#3D8CAE",
+      },
+      {
+        id: 5,
+        buttonColor: "#63913F",
+      },
+      {
+        id: 6,
+        buttonColor: "#FBAC5A",
+      },
+      {
+        id: 7,
+        buttonColor: "#cc33ff",
+      },
+      {
+        id: 8,
+        buttonColor: "#00ff00",
+      },
+    ],
+    sizes: [
+      {
+        id: 1,
+        size: "xxs",
+      },
+      {
+        id: 2,
+        size: "xs",
+      },
+      {
+        id: 3,
+        size: "s",
+      },
+      {
+        id: 4,
+        size: "m",
+      },
+      {
+        id: 5,
+        size: "l",
+      },
+      {
+        id: 6,
+        size: "xl",
+      },
+      {
+        id: 7,
+        size: "xxl",
+      },
+    ],
+  },
+];
