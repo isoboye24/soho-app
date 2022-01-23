5691d6cc55061bcc83fa1274a75ef353f219907c
isoboye24 <isobarguy4real@gmail.com>
2022-01-23 15:57:31 +0200

    I setup the grid structure

--- src/component/All-product.js
+++ src/component/All-product.js
@@ -3,11 +3,13 @@ import React from "react";
 const AllProducts = () => {
   return (
     <>
-      <div className="row">
-        <div className="col-1-of-4">I am a col 1</div>
-        <div className="col-1-of-4">I am a col 2</div>
-        <div className="col-1-of-4">I am a col 3</div>
-        <div className="col-1-of-4">I am a col 4</div>
+      <div className="container">
+        <div className="row">
+          <div className="">I am a col 1</div>
+          <div className="">I am a col 2</div>
+          <div className="">I am a col 3</div>
+          <div className="">I am a col 4</div>
+        </div>
       </div>
     </>
   );
